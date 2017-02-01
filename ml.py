a71db0cd4f97e966dc7302da8688b6bb64d47027
Ashu Dagar <dagar.ashu053@gmail.com>
2017-02-01 14:20:45 +0530

    test case added

--- ml.py
+++ ml.py
@@ -34,3 +34,24 @@ dataset.hist()
 # scatter plot matrix
 scatter_matrix(dataset)
 #plt.show()
+
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
